7a7502ec8f9ae944773d0df1312d4b9a3986ea59
httpshotmaker <httpshotmaker@vk.com>
2023-12-23 09:13:14 +0300

    Working version

--- main.py
+++ main.py
@@ -1,102 +1,83 @@
-from fastapi.openapi.models import Info
+from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.responses import JSONResponse
-from datetime import timedelta
-from typing import Annotated, Union
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
 
-from fastapi import Depends, FastAPI, HTTPException, status, Cookie
-from fastapi.security import OAuth2PasswordRequestForm
-from classes.data import User, Token, UserInDB, UserRegister
-from functions.db import AuthHandler, fake_users_db
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
+"""
+Note: This is just a basic example how to enable cookies.
+This is vulnerable to CSRF attacks, and should not be used this example.
+"""
 
 app = FastAPI()
 
-auth_handler = AuthHandler()
+class User(BaseModel):
+    email: str
+    password: str
 
+class Settings(BaseModel):
+    authjwt_secret_key: str = "secret"
+    authjwt_token_location: set = {"cookies"}
+    authjwt_cookie_csrf_protect: bool = False
 
-@app.post("/token", response_model=Token)
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    user = auth_handler.authenticate_user(fake_users_db, form_data.email, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
-    access_token = auth_handler.create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
     )
-    return {"access_token": access_token, "token_type": "bearer"}
 
+@app.post('/login')
+def login(user: User, Authorize: AuthJWT = Depends()):
+    if user.email != "test" or user.password != "test":
+        raise HTTPException(status_code=401,detail="Bad email or password")
 
-@app.get("/users/me/")
-async def read_users_me(token: str = Cookie(default=None)):
-    user = auth_handler.get_current_user(token=token)
-    print(user)
-    return JSONResponse(content=user, status_code=status.HTTP_200_OK)
+    access_token = Authorize.create_access_token(subject=user.email)
+    if Authorize._verify_and_get_jwt_in_cookies('refresh', Authorize._request):
+        Authorize.unset_jwt_cookies()
+    refresh_token = Authorize.create_refresh_token(subject=user.email)
 
+    Authorize.set_access_cookies(access_token)
+    Authorize.set_refresh_cookies(refresh_token)
+    return {"msg":"Successfully login"}
 
-@app.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[UserRegister, Depends(auth_handler.get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.email}]
+@app.post('/refresh')
+def refresh(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_refresh_token_required()
 
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    Authorize.set_access_cookies(new_access_token)
+    return {"msg":"The token has been refresh"}
 
-@app.post(
-    "/register",
-    responses={
-        201: {"description": "User registered successfully"},
-        400: {"description": "User already exists"},
-        500: {"description": "Internal server error"},
-    },
-)
-async def register(user: UserRegister):
-    if auth_handler.get_user(username=user.email) is not None:
-        raise HTTPException(status_code=400, detail="User already exists")
+@app.delete('/logout')
+def logout(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
 
-    try:
-        hashed_password = auth_handler.get_password_hash(user.password)
+    Authorize.unset_jwt_cookies()
+    return {"msg":"Successfully logout"}
 
-        new_user = UserInDB(
-            email=user.email,
-            name=user.name,
-            disabled=False,
-            interests=user.interests,
-            gift_preferences=user.gift_preferences,
-            hashed_password=hashed_password,
-        )
+@app.get('/protected')
+def protected(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
 
-        access_token_expires = timedelta(minutes=int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')))
-        access_token = auth_handler.create_access_token(
-            data={"sub": new_user.email}, expires_delta=access_token_expires
-        )
+    current_user = Authorize.get_jwt_subject()
+    return {"user": current_user}
 
-        response = {"message": "User registered successfully"}
-        response_headers = {
-            "Set-Cookie": f"token={access_token}; HttpOnly; Secure; SameSite=None; Max-Age=600"
-        }
-        return JSONResponse(content=response, headers=response_headers, status_code=201)
+@app.post('/register')
+def register(user: User, Authorize: AuthJWT = Depends()):
+    if user.email == "test":
+        raise HTTPException(status_code=401,detail="User already exists")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+    access_token = Authorize.create_access_token(subject=user.email)
+    refresh_token = Authorize.create_refresh_token(subject=user.email)
 
+    Authorize.set_access_cookies(access_token)
+    Authorize.set_refresh_cookies(refresh_token)
 
-@app.get("/auth/")
-async def read_items(token: Annotated[str, Depends(auth_handler.oauth2_scheme)]):
-    return JSONResponse(headers={"Set-Cookie": f"token={token}; Path=/;"})
-
-
-if __name__ == "__main__":
-    openapi_info = Info(
-        title="Secret Santa API",
-        version="1.0.0",
-        description="API for the Secret Santa site",
-    )
+    # Верни сообщение об успешной регистрации
+    return {"msg": "Successfully registered"}
