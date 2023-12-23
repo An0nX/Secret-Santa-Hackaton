2a90c0a49ed40d89c817a5d0e757cba5c2f30feb
st0rmey3 <vodolazko.aleksandr@gmail.com>
2023-12-23 14:57:14 +0300

    Adding documentation to main func

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import translators as ts
 API_KEY = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"
 
 def suggest_gift(preferences: str, budget: int = 10):
+    """
+    Generate a gift suggestion based on the user's preferences and budget.
+
+    Parameters:
+    - preferences (str): The user's preferences for the gift.
+    - budget (int, optional): The budget for the gift. Defaults to 10.
+
+    Returns:
+    - None
+    """
     response = httpx.post("https://api.promptjoy.com/api/jQGCwq", json={"interest": preferences, "budget": f"{budget}"}, headers={'Authorization': 'Bearer sk-17c4f2e94891fae6423fde005b9064d74372a564'}).json()
     translated = ts.translate_text(query_text=response["gift"], translator="google", from_language="en", to_language="ru", api_key=API_KEY, region='ru')
     print(translated)
